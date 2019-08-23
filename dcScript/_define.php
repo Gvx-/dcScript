6bb6dec47c41eed0f7c19faca80c6cbb459ec175
Gvx <gvx@wanadoo.fr>
2019-08-23 22:17:28 +0200

    Mise a jour support

--- dcScript/_define.php
+++ dcScript/_define.php
@@ -11,14 +11,14 @@ $this->registerModule(
 	/* Name */			'dcScript',
 	/* Description*/	'Add script for DC 2.8+',
 	/* Author */		'Gvx',
-	/* Version */		'2.0.1',
+	/* Version */		'2.0.2',
 	array(
 		/* standard plugin options dotclear */
 		'permissions' 				=>	'dcScript.edit'
 		, 'type'					=>	'plugin'
 		, 'Priority'				=>	1000
 		, 'support'		/* url */	=>	'http://forum.dotclear.org/viewtopic.php?pid=335785#p335785'
-		, 'details' 	/* url */	=>	'https://bitbucket.org/Gvx_/dcscript'
+		, 'details' 	/* url */	=>	'https://github.com/Gvx-/dcScript'
 		, 'requires'	/* id(s) */	=>	array(
 			array('core', '2.8')
 		)
